e106248de3dc03e5cbf2d0da488eb47bb27207af
raissaguimaraes <raissaguimaraes@live.com>
2022-05-18 15:37:17 -0300

    Login Page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+import React from 'react'
+import Layout from '../components/layout'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <React.Fragment>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </React.Fragment>
+  )
 }
 
 export default MyApp
